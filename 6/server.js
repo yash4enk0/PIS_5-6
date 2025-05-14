65c206a278b41b27cce462fa7d011da34d40fe5b
yash4enk0 <oleksandra.yash4enk0@gmail.com>
2025-05-14 20:49:56 +0300

    lab 6 done

---
+++ 6/server.js
@@ -0,0 +1,37 @@
+const WebSocket = require('ws');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    const filePath = path.join(__dirname, 'index.html');
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        return res.end('Error loading index.html');
+      }
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    });
+  }
+});
+
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', ws => {
+  ws.on('message', message => {
+    wss.clients.forEach(client => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message.toString());
+      }
+    });
+  });
+
+  ws.send('✅ Підключено до корпоративного чату.');
+});
+
+const PORT = 8080;
+server.listen(PORT, () => {
+  console.log(`Сервер запущено на http://localhost:${PORT}`);
+});
